11c969acb45bf27bcf373a5b15be461f6d3365d6
rajato1209 <rajato1209@gmail.com>
2023-01-12 23:44:37 +0530

    portfolio

--- my-portfolio/src/Components/Projects.jsx
+++ my-portfolio/src/Components/Projects.jsx
@@ -1,13 +1,13 @@
-import React, {useEffect} from 'react';
-import styled from 'styled-components';
-import decanhearaldPng from '../Resources/Images/Project/decanhearaldPng.png';
-import AirgaragePng from '../Resources/Images/Project/AirgaragePng.png';
-import youtubePng from '../Resources/Images/Project/youtubePng.png';
-import wrdrbPng from '../Resources/Images/Project/wrdrbPng.png';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import StreamIcon from '@mui/icons-material/Stream';
-import { useState } from 'react';
-import { Link } from "@chakra-ui/react";
+// import React, {useEffect} from 'react';
+// import styled from 'styled-components';
+// import decanhearaldPng from '../Resources/Images/Project/decanhearaldPng.png';
+// import AirgaragePng from '../Resources/Images/Project/AirgaragePng.png';
+// import youtubePng from '../Resources/Images/Project/youtubePng.png';
+// import wrdrbPng from '../Resources/Images/Project/wrdrbPng.png';
+// import GitHubIcon from '@mui/icons-material/GitHub';
+// import StreamIcon from '@mui/icons-material/Stream';
+// import { useState } from 'react';
+// import { Link } from "@chakra-ui/react";
 
 
 // const Projects = () => {
@@ -99,323 +99,332 @@ import { Link } from "@chakra-ui/react";
 // }
 
 
-const Container = styled.div`
-    width:100%;
-    margin:auto;
-    display:flex;
-    flex-direction:column;
-    gap:20px;
+// const Container = styled.div`
+//     width:100%;
+//     margin:auto;
+//     display:flex;
+//     flex-direction:column;
+//     gap:20px;
     
-    h1{
-        color:teal;
-        text-transform:uppercase;
-    }
+//     h1{
+//         color:teal;
+//         text-transform:uppercase;
+//     }
 
-    .projectsContainer{
-        width:80%;
-        margin:auto;
-        display:flex;
-        align-items:center;
-        flex-direction:column;
-    }
+//     .projectsContainer{
+//         width:80%;
+//         margin:auto;
+//         display:flex;
+//         align-items:center;
+//         flex-direction:column;
+//     }
 
-    .box{
-        position:relative;
-        width:100%;
-        height:450px;
-        display:flex;
-        gap:10px;
-        margin-bottom:80px;
-        border: 10px solid #e9e9e9;
-        border-radius: 20px;
-        overflow:hidden;
-    }
+//     .box{
+//         position:relative;
+//         width:100%;
+//         height:450px;
+//         display:flex;
+//         gap:10px;
+//         margin-bottom:80px;
+//         border: 10px solid #e9e9e9;
+//         border-radius: 20px;
+//         overflow:hidden;
+//     }
 
-    .imgBox{
-        width:50%;
-        overflow:hidden;
-    }
-
-    
-    .imgBox>img{
-        display: block;
-        transition: transform .4s; 
-    }
-
-    .imgBox>img:hover{
-        position:absolute;
-        width:80%;
-        transform: scale(1.3);
-        transform-origin: 0% 0%;
-    }
+//     .imgBox{
+//         width:50%;
+//         overflow:hidden;
+//     }
 
     
-    .ContentBox{
-        width:50%;
-        text-align:left;
-        padding:0px 20px 20px 20px;
+//     .imgBox>img{
+//         display: block;
+//         transition: transform .4s; 
+//     }
+
+//     .imgBox>img:hover{
+//         position:absolute;
+//         width:80%;
+//         transform: scale(1.3);
+//         transform-origin: 0% 0%;
+//     }
+
+    
+//     .ContentBox{
+//         width:50%;
+//         text-align:left;
+//         padding:0px 20px 20px 20px;
         
-    }
+//     }
 
-    .ContentBox>h1{
-        color:#343a40;
-    }
+//     .ContentBox>h1{
+//         color:#343a40;
+//     }
 
-    .ContentBox>p{
-        width:90%;
-        font-size:large;
-        margin-top:0px;
-    }
+//     .ContentBox>p{
+//         width:90%;
+//         font-size:large;
+//         margin-top:0px;
+//     }
 
-    .tagsBox{
-        width:80%;
-        display:flex;
-        align-items:center;
-        flex-wrap: wrap;
-        line-height:20px;
-        gap:5px;
-        margin-bottom:20px;
-    }
+//     .tagsBox{
+//         width:80%;
+//         display:flex;
+//         align-items:center;
+//         flex-wrap: wrap;
+//         line-height:20px;
+//         gap:5px;
+//         margin-bottom:20px;
+//     }
 
-    .tagsBox>span{
-        color:white;
-        background-color:teal;
-        padding: 2px 15px 2px 15px;
-        border-radius:10px;
-        font-size:13px;
-        font-weight:bold;
-    }
+//     .tagsBox>span{
+//         color:white;
+//         background-color:teal;
+//         padding: 2px 15px 2px 15px;
+//         border-radius:10px;
+//         font-size:13px;
+//         font-weight:bold;
+//     }
 
 
-    .linkSection{
-        display:flex;
-        gap:20px;
-    }
+//     .linkSection{
+//         display:flex;
+//         gap:20px;
+//     }
 
-    .linkSection>a{
-        text-decoration:none;
-    }
-    .linkSection>a:first-child>button{
-        background-color:black;
-        color: White;
-        padding:8px 20px 8px 20px;
-        border-radius:8px;
-        font-weight:bold;
-        display:flex;
-        align-items:center;
-        justify-content:center;
-        gap:10px;
-        border:none;
-        cursor:pointer;
-    }
+//     .linkSection>a{
+//         text-decoration:none;
+//     }
+//     .linkSection>a:first-child>button{
+//         background-color:black;
+//         color: White;
+//         padding:8px 20px 8px 20px;
+//         border-radius:8px;
+//         font-weight:bold;
+//         display:flex;
+//         align-items:center;
+//         justify-content:center;
+//         gap:10px;
+//         border:none;
+//         cursor:pointer;
+//     }
 
-    button{
-        cursor:pointer;
-    }
+//     button{
+//         cursor:pointer;
+//     }
 
-    button:hover{
-        -webkit-transform: translateY(-2px);
-        transform: translateY(-2px);
-        -webkit-transition: all 0.5s;
-        transition: all 0.5s;
-    }
+//     button:hover{
+//         -webkit-transform: translateY(-2px);
+//         transform: translateY(-2px);
+//         -webkit-transition: all 0.5s;
+//         transition: all 0.5s;
+//     }
     
 
-    .linkSection>a:last-child>button{
-        background-color:#44a4be;
-        color: White;
-        padding:8px 20px 8px 20px;
-        border-radius:8px;
-        border:none;
-        font-weight:bold;
-        display:flex;
-        align-items:center;
-        justify-content:center;
-        gap:10px;
-    }
+//     .linkSection>a:last-child>button{
+//         background-color:#44a4be;
+//         color: White;
+//         padding:8px 20px 8px 20px;
+//         border-radius:8px;
+//         border:none;
+//         font-weight:bold;
+//         display:flex;
+//         align-items:center;
+//         justify-content:center;
+//         gap:10px;
+//     }
 
-    @media only screen and (min-width: 768px) and (max-width:1120px){
-        .box{
-            flex-direction:column;
-            align-items:center;
-            height:100%;
-        }
+//     @media only screen and (min-width: 768px) and (max-width:1120px){
+//         .box{
+//             flex-direction:column;
+//             align-items:center;
+//             height:100%;
+//         }
  
-        .projectsContainer{
-            width:80%;
-            margin:auto;
+//         .projectsContainer{
+//             width:80%;
+//             margin:auto;
             
-        }
+//         }
         
-        .imgBox{
-            width:100%;
-            height:400px;
-        }
+//         .imgBox{
+//             width:100%;
+//             height:400px;
+//         }
 
-        .imgBox>img{
-            width:100%;
-        }
+//         .imgBox>img{
+//             width:100%;
+//         }
 
-        .ContentBox{
-            width:80%;
-            display:flex;
-            align-items:center;
-            justify-content:center;
-            flex-direction:column;
-        }
+//         .ContentBox{
+//             width:80%;
+//             display:flex;
+//             align-items:center;
+//             justify-content:center;
+//             flex-direction:column;
+//         }
 
-        .ContentBox>h1{
-            font-size:x-large;
-        }
+//         .ContentBox>h1{
+//             font-size:x-large;
+//         }
 
-        .ContentBox>p{
-            font-size:15px;
-        }
+//         .ContentBox>p{
+//             font-size:15px;
+//         }
 
-        .tagsBox{
-            width:100%;
-            justify-content:center;
-        }
-    }
+//         .tagsBox{
+//             width:100%;
+//             justify-content:center;
+//         }
+//     }
 
-    @media only screen and (min-width: 481px) and (max-width:768px){
-        .box{
-           flex-direction:column;
-           align-items:center;
-           height:100%;
-        }
+//     @media only screen and (min-width: 481px) and (max-width:768px){
+//         .box{
+//            flex-direction:column;
+//            align-items:center;
+//            height:100%;
+//         }
 
-        .projectsContainer{
-            width:80%;
-            margin:auto;
+//         .projectsContainer{
+//             width:80%;
+//             margin:auto;
             
-        }
+//         }
         
-        .imgBox{
-            width:100%;
-            height:280px;
-        }
+//         .imgBox{
+//             width:100%;
+//             height:280px;
+//         }
 
-        .imgBox>img{
-            width:100%;
-        }
+//         .imgBox>img{
+//             width:100%;
+//         }
 
-        .ContentBox{
-            width:80%;
-            display:flex;
-            align-items:center;
-            justify-content:center;
-            flex-direction:column;
-            text-align:center;
-        }
+//         .ContentBox{
+//             width:80%;
+//             display:flex;
+//             align-items:center;
+//             justify-content:center;
+//             flex-direction:column;
+//             text-align:center;
+//         }
 
-        .ContentBox>h1{
-            font-size:x-large;
-        }
+//         .ContentBox>h1{
+//             font-size:x-large;
+//         }
 
-        .ContentBox>p{
-            font-size:15px;
-        }
+//         .ContentBox>p{
+//             font-size:15px;
+//         }
 
-        .tagsBox{
-            width:100%;
-            justify-content:center;
-        }
-    }
+//         .tagsBox{
+//             width:100%;
+//             justify-content:center;
+//         }
+//     }
 
 
-    @media only screen and (min-width:320px) and (max-width:480px){
-        .box{
-            flex-direction:column;
-            align-items:center;
-            height:100%;
-        }
+//     @media only screen and (min-width:320px) and (max-width:480px){
+//         .box{
+//             flex-direction:column;
+//             align-items:center;
+//             height:100%;
+//         }
 
-        .projectsContainer{
-            width:90%;
-            margin:auto;
+//         .projectsContainer{
+//             width:90%;
+//             margin:auto;
             
-        }
+//         }
 
-        .imgBox{
-            width:100%;
-            height:200px;
-        }
+//         .imgBox{
+//             width:100%;
+//             height:200px;
+//         }
 
-        .imgBox>img{
-            width:100%;
-        }
+//         .imgBox>img{
+//             width:100%;
+//         }
 
-        .ContentBox{
-            width:80%;
-            display:flex;
-            align-items:center;
-            justify-content:center;
-            flex-direction:column;
-            text-align:center;
-        }
+//         .ContentBox{
+//             width:80%;
+//             display:flex;
+//             align-items:center;
+//             justify-content:center;
+//             flex-direction:column;
+//             text-align:center;
+//         }
 
-        .ContentBox>h1{
-            font-size:large;
-        }
+//         .ContentBox>h1{
+//             font-size:large;
+//         }
 
-        .ContentBox>p{
-            font-size:13px;
+//         .ContentBox>p{
+//             font-size:13px;
             
-        }
+//         }
 
 
-        .tagsBox{
-            width:100%;
-            justify-content:center;
-        }
-    }
+//         .tagsBox{
+//             width:100%;
+//             justify-content:center;
+//         }
+//     }
 
-    @media only screen and (max-width: 319px){
-        .box{
-            flex-direction:column;
-            align-items:center;
-            height:100%;
-        }
+//     @media only screen and (max-width: 319px){
+//         .box{
+//             flex-direction:column;
+//             align-items:center;
+//             height:100%;
+//         }
 
-        .projectsContainer{
-            width:90%;
-            margin:auto;
+//         .projectsContainer{
+//             width:90%;
+//             margin:auto;
             
-        }
+//         }
 
-        .imgBox{
-            width:100%;
-            height:150px;
-        }
+//         .imgBox{
+//             width:100%;
+//             height:150px;
+//         }
 
-        .imgBox>img{
-            width:100%;
+//         .imgBox>img{
+//             width:100%;
             
-        }
+//         }
 
-        .ContentBox{
-            width:80%;
-            display:flex;
-            align-items:center;
-            justify-content:center;
-            flex-direction:column;
-            text-align:center;
-        }
+//         .ContentBox{
+//             width:80%;
+//             display:flex;
+//             align-items:center;
+//             justify-content:center;
+//             flex-direction:column;
+//             text-align:center;
+//         }
 
-        .ContentBox>h1{
-            font-size:large;
-        }
+//         .ContentBox>h1{
+//             font-size:large;
+//         }
 
-        .ContentBox>p{
-            font-size:13px;
-        }
+//         .ContentBox>p{
+//             font-size:13px;
+//         }
 
 
-        .tagsBox{
-            width:100%;
-            justify-content:center;
-        }
-    }
-`
-export default Projects
+//         .tagsBox{
+//             width:100%;
+//             justify-content:center;
+//         }
+//     }
+// `
+// export default Projects
+ import React from 'react'
+ 
+ const Projects = () => {
+   return (
+     <div>Projects</div>
+   )
+ }
+ 
+ export default Projects
